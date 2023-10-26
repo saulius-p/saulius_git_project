5050a35ca7e41c59b622fae0385b5df68d4c3074
Saulius P <saulius.t.paukstys@gmail.com>
2023-10-26 15:14:30 +0300

    Atlikta dar viena papildoma pratimo dalis.
    Spausdinsime tik lyginius skaičius, kurie dalinasi iš 9.

--- pirmas.py
+++ pirmas.py
@@ -50,3 +50,15 @@ for sk in range(1, 101):
     if sk == sustoti:
         break
 print("")
+
+# Praleidžiame visus skaičius (iki vartotojo pasirinkimo) išskyrus tuos, kurie dalinasi iš 2 ir 9. Bus 18 ir 36 it t.t.
+
+sustoti = int(input("Įveskite skaičių, ties kuriuo nustoti tyrinėti seką: "))
+for sk in range(1, 101):
+    if sk == sustoti:
+        break
+    if not (sk % 2 == 0 and sk % 9 == 0):
+        continue
+    print(sk, end=" | ")
+print("")
+
