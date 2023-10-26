8060a51af66f274f3aa38acb817334600a24af97
Saulius P <saulius.t.paukstys@gmail.com>
2023-10-26 15:02:23 +0300

    Atlikta trečia pratimo dalis. Bus praleisti pirmieji 10 skaičių.

--- pirmas.py
+++ pirmas.py
@@ -11,3 +11,11 @@ for sk in range(1, 101):
     if sk == 20:
         break
 print("")
+
+# 1.3
+
+for sk in range(1, 101):
+    if sk in range(1, 11):
+        continue
+    print(sk, end=" | ")
+print("")
